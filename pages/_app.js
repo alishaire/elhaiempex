4e043a78eefbe498efa2b057e8593db89c3af6eb
iMac <imac@iMacs-iMac.local>
2023-12-20 13:45:23 +0500

    adding socail media icon on the layout of the website

--- pages/_app.js
+++ pages/_app.js
@@ -14,12 +14,29 @@ import "@/styles/Team.css";
 import "@/styles/Home.css";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
   return (
     <>
       <PrimeReactProvider>
         <Navbar />
+        {pathname != "/contact" && pathname != "/about" && (
+          <div className="AllPageWhatsApp">
+            <Link
+              href="whatsapp://send?phone=+923186187272"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <i className="fa-brands fa-whatsapp"></i>
+            </Link>
+          </div>
+        )}
+
         <Component {...pageProps} />
       </PrimeReactProvider>
     </>
